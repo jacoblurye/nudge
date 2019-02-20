4016a75f46b1ca8097d75d5615650968da922f29
Jacob Lurye <jlurye@college.harvard.edu>
2019-02-19 20:17:16 -0500

    Add note on Target URL

--- src/components/TargetSection/index.tsx
+++ src/components/TargetSection/index.tsx
@@ -3,6 +3,9 @@ import TargetURLInput from "../TargetURLInput";
 import AppStateContext from "../AppStateContext";
 import TargetURLItem from "../TargetURLItem";
 
+/** The Target URL is the URL that the user wants to focus on.
+ * All "blocked" URLs will redirect to the target URL.
+ */
 const TargetSection = () => {
   const { appState } = React.useContext(AppStateContext)!;
 
