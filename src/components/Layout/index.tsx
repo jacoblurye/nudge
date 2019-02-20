3752ef12264468db9372219b19e374465dcc65e3
Jacob Lurye <jlurye@college.harvard.edu>
2019-02-19 21:07:27 -0500

    Add dimmer on disable

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
-import { Divider, Grid, Header, Checkbox } from "semantic-ui-react";
+import {
+  Divider,
+  Grid,
+  Header,
+  Checkbox,
+  Dimmer,
+  Transition,
+  Icon
+} from "semantic-ui-react";
 import BlockedSection from "../../components/BlockedSection";
 import TargetSection from "../../components/TargetSection";
 import AppStateContext from "../AppStateContext";
@@ -8,28 +16,38 @@ const Layout = () => {
   const { appState, toggleEnabled } = React.useContext(AppStateContext)!;
 
   return (
-    <Grid padded columns={2} style={{ width: "400px" }}>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column>
-          <Header size="medium">Nudge</Header>
-        </Grid.Column>
-        <Grid.Column floated="right">
-          <Checkbox
-            toggle
-            style={{ float: "right" }}
-            checked={appState.enabled}
-            onChange={toggleEnabled}
-          />
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row centered>
-        <TargetSection />
-      </Grid.Row>
-      <Divider />
-      <Grid.Row centered>
-        <BlockedSection />
-      </Grid.Row>
-    </Grid>
+    <div>
+      <Grid padded columns={2} style={{ width: "400px" }}>
+        <Grid.Row verticalAlign="middle">
+          <Grid.Column>
+            <Header size="medium">Nudge</Header>
+          </Grid.Column>
+          <Grid.Column floated="right">
+            <Checkbox
+              toggle
+              style={{ float: "right" }}
+              checked={appState.enabled}
+              onChange={toggleEnabled}
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Grid as={Dimmer.Dimmable} padded columns={2} style={{ width: "400px" }}>
+        <Grid.Row centered>
+          <TargetSection />
+        </Grid.Row>
+        <Divider />
+        <Grid.Row centered>
+          <BlockedSection />
+        </Grid.Row>
+        <Transition visible={!appState.enabled}>
+          <Dimmer active={!appState.enabled}>
+            <Icon name="moon" />
+            Nudge is disabled
+          </Dimmer>
+        </Transition>
+      </Grid>
+    </div>
   );
 };
 
