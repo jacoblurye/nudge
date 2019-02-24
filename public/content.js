b80cfdbcc9ff188cad33b6614bc3929a94592271
Jacob Lurye <jlurye209@gmail.com>
2019-02-24 09:59:30 -0500

    Fix deactivation

--- public/content.js
+++ public/content.js
@@ -1,7 +1,7 @@
 /* global chrome */
 
 chrome.storage.sync.get(storage => {
-  if (window.location.hostname in storage)
+  if (window.location.hostname in storage && storage.enabled === true)
     chrome.storage.sync.get(["targetURL"], ({ targetURL }) => {
       if (!!targetURL) window.location.href = targetURL;
     });
